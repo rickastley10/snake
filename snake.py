669b9d17464eecb22469a40c9960e7df8cd42c04
rickastley10 <148710838+rickastley10@users.noreply.github.com>
2026-02-02 11:02:47 +0000

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,24 +1,35 @@
 import turtle as t
 import random
-t.hideturtle()
+
 facing = "up"
 t.penup()
 px, py = 0, 0
 segments = []
 t.title("snake")
-bx = (random.randint(-10, 10))*20
-by = (random.randint(-10, 10))*20
+bx = (random.randint(-10, 10)) * 20
+by = (random.randint(-10, 10)) * 20
 segc = 4
-score = 0
-highscore = 0
 t.tracer(0, 0)
+t.hideturtle()
 
 for _ in range(segc):
     segments.append({
         "sx": 0,
         "sy": 0
     })
-    
+
+menu = 1
+
+def menu1():
+    if menu == 1:
+        t.goto(-100, 0)
+        t.pencolor("red")
+        t.write("snake game\n        click anywhere to start", font=("Arial", 16, "normal"))
+    if menu == 0:
+        mainloop()
+
+menu1()
+
 def snake():
     t.fillcolor("black")
     t.goto(px, py)
@@ -29,8 +40,8 @@ def snake():
         t.right(90)
     t.end_fill()
     t.penup()
-    for _ in segments:
-        t.goto(_["sx"], _["sy"])
+    for seg in segments:
+        t.goto(seg["sx"], seg["sy"])
         t.pendown()
         t.begin_fill()
         for _ in range(4):
@@ -39,41 +50,31 @@ def snake():
         t.end_fill()
         t.penup()
 
-    
 def snakemove():
     global px, py
     global bx, by
-    global segc, segments, score
+    global segc, segments
+
     if facing == "up":
         segments.insert(0, {"sx": px, "sy": py})
-        
-        
         py += 20
     elif facing == "down":
         segments.insert(0, {"sx": px, "sy": py})
-        
-        
         py -= 20
     elif facing == "left":
         segments.insert(0, {"sx": px, "sy": py})
-        
-        
         px -= 20
     elif facing == "right":
         segments.insert(0, {"sx": px, "sy": py})
-        
-        
         px += 20
+
     if px == bx and py == by:
-        
-        bx = (random.randint(-10, 10))*20
-        by = (random.randint(-10, 10))*20
-        score += 1
+        bx = (random.randint(-10, 10)) * 20
+        by = (random.randint(-10, 10)) * 20
     else:
         segments.pop()
 
     if any(px == seg["sx"] and py == seg["sy"] for seg in segments):
-
         segc = 4
         segments.clear()
         segments = []
@@ -82,11 +83,8 @@ def snakemove():
                 "sx": 0,
                 "sy": 0
             })
-
         px = 0
         py = 0
-        score = 0
-    
 
 def berry():
     t.goto(bx, by)
@@ -98,64 +96,50 @@ def berry():
         t.right(90)
     t.end_fill()
     t.penup()
-    
-def display():
-    global highscore
-    if score > highscore:
-        highscore = score
-    t.goto(-200, -200)
-    t.write(f"score: {score}\n highscore: {highscore}", font=("Arial", 8, "normal"))
 
-def right():
+def go_right():
     global facing
     facing = "right"
 
-def left():
+def go_left():
     global facing
     facing = "left"
 
-def up():
+def go_up():
     global facing
     facing = "up"
 
-def down():
+def go_down():
     global facing
     facing = "down"
 
 def click(x, y):
-    if y < 0 and 100 > x > -100:
-        down()
-    if y > 0 and 100 > x > -100:
-        up()
-    if x < 0 and 100 > y > -100:
-        left()
-    if x > 0 and 100 > y > -100:
-        right()
+    global menu
+    if menu == 1:
+        menu = 0
+        menu1()
+    if menu == 0:
+        if y < 0 and 100 > x > -100:
+            go_down()
+        if y > 0 and 100 > x > -100:
+            go_up()
+        if x < 0 and 100 > y > -100:
+            go_left()
+        if x > 0 and 100 > y > -100:
+            go_right()
 
-t.onkey(up, "Up")
-t.onkey(down, "Down")
-t.onkey(left, "Left")
-t.onkey(right, "Right")
-
-t.onkey(up, "w")
-t.onkey(down, "s")
-t.onkey(left, "a")
-t.onkey(right, "d")
-
+t.onkey(go_up, "Up")
+t.onkey(go_down, "Down")
+t.onkey(go_left, "Left")
+t.onkey(go_right, "Right")
 t.onscreenclick(click)
-
 t.listen()
 
 def mainloop():
     t.clear()
-
     snake()
     berry()
     snakemove()
-    display()
-
     t.ontimer(mainloop, 300)
-
-mainloop()
 
 t.mainloop()
